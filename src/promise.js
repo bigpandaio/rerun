ec43caab379dd38f018436204ddabb3a6aced87b
Shahar Kedar <shahar@gmail.com>
2015-01-06 16:51:06 +0200

    Added support for passing logger as option to rerun execution

--- src/promise.js
+++ src/promise.js
@@ -7,6 +7,8 @@ module.exports = function (toRetry, options) {
   var retries = options.retries || 3;
   var timeout = options.retryTimeout || 50;
   var factor = options.retryFactor || 1;
+  var logger = options.logger || require('log-devnul');
+  var identifier = options.identifier
   var userRejectError = options.rejectError || RejectError;
   var randomizeRetry = options.retryRandom || false;
 
@@ -16,9 +18,11 @@ module.exports = function (toRetry, options) {
 
   function _failed(error) {
     if (error instanceof RejectError || error instanceof userRejectError || --retries <= 0) {
+      logger.error('Function execution failed permanently', {identifier: identifier})
       deferred.reject(error);
     } else {
-      var timeToWait = randomizeRetry ? Math.random() * (timeout / 2) + (timeout / 2) : timeout; 
+      var timeToWait = randomizeRetry ? Math.random() * (timeout / 2) + (timeout / 2) : timeout;
+      logger.warn('Function execution failed', {retries: retries, wait_for: timeToWait, identifier: identifier})
       Q.delay(timeToWait).then(function () { return toRetry() }).then(_succeed, _failed);
       timeout *= factor;
     }
--- test/unit/promise.spec.js
+++ test/unit/promise.spec.js
@@ -1,7 +1,10 @@
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 var expect = chai.expect;
+var sinon = require('sinon')
+var sinonChai = require('sinon-chai')
 chai.use(chaiAsPromised);
+chai.use(sinonChai);
 
 
 var rerun= require('../../src/index').promise;
@@ -43,6 +46,35 @@ describe('Promise retry tests', function () {
       var retries = 2;
       var promise = rerun(function() { return --retries }, {retries: 2, retryFactor: 0, retryTimeout: 10});
       return expect(promise).to.eventually.equal(1);
+    })
+
+  })
+
+  describe('passing logger in options', function() {
+
+    var logger = require('log-devnul');
+    var spy = sinon.spy(logger, 'warn');
+
+    it('should result in calls to logger', function() {
+        var promise = rerun(function() { return Q.reject(new Error()) }, {retries: 2, retryFactor: 1, retryTimeout: 10, logger: logger});
+        return promise.fail(function() {
+          expect(spy).to.be.called
+          return Q();
+        })
+    })
+
+    describe('passing identifier in options', function() {
+
+      it('should result in identifier being in log message metadata', function() {
+        var identifier = 'test';
+        var promise = rerun(function() { return Q.reject(new Error()) }, {retries: 2, retryFactor: 1, retryTimeout: 10, logger: logger, identifier: identifier});
+        return promise.fail(function() {
+          expect(spy).to.be.calledWithMatch({}, {identifier: identifier})
+          return Q();
+        })
+
+      })
+
     })
 
   })
